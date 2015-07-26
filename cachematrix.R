0584381e09eed31321fe1563e04c11bdf633a93e
Brett Amdur <brett.amdur@gmail.com>
2015-07-26 16:03:35 -0400

    deleted fake.txt / uploaded first prelim version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+##################################################################################################
+## The two functions below create a process for making  an "object" (it is actually 		##
+## an R environment) that contains a matrix, its inverse, and a set of functions that		## 
+## permit the user to interact with the object to get and set the matrix, and get and set 	##
+## the matrix's inverse.									##
+##################################################################################################
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a closure.  It takes a matrix as an argument. 
+## It returns a list of functions, but also remains resident
+## in memory 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	matrixInverse <- NULL 			
+	setMatrix <- function(y) {			
+		x <<- y  			
+		matrixInverse <<- NULL  	
+	}
+	getMatrix <- function() x  		
+	setInverse <- function(inverse) matrixInverse <<- inverse  
+	getInverse <- function() matrixInverse 		
+	return(list(setMatrix = setMatrix, 
+		    getMatrix = getMatrix,    
+		    setInverse = setInverse,
+		    getInverse = getInverse))
 }
 
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getInverse()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}	
+	data <- x$getMatrix()
+	i <- solve(data, ...)
+	x$setInverse(i)
+	i
 }
